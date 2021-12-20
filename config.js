d10e77ccdb5e2cc13516e118ff4484d9b116a9bf
divyanshipatel123 <divyanshipatel2008apr13@gmail.com>
2021-12-20 14:32:13 +0530

    Spectagram all commits

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAWzgBcsrVbo4JA63CINIMpXHkLHZv6KrE",
+  authDomain: "spectagram-df184.firebaseapp.com",
+  databaseURL: "https://spectagram-df184-default-rtdb.firebaseio.com",
+  projectId: "spectagram-df184",
+  storageBucket: "spectagram-df184.appspot.com",
+  messagingSenderId: "1075913159481",
+  appId: "1:1075913159481:web:f78064e8eacc770c2c2ee8"
+};
